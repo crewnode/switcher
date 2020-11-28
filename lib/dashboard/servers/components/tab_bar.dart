b8db5f14e41425c0b8d80dc39dbcaea82c50ceba
Reece Benson <business@reecebenson.me>
2020-11-28 03:43:34 +0000

    Tab Bar for servers

---
+++ lib/dashboard/servers/components/tab_bar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TabBar extends StatelessWidget {
+  const TabBar({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(),
+    );
+  }
+}
